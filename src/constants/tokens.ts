37f1d00aac70c67b5a317b27fa364b7770512ed5
SHIBAINUNI <94232115+SHIBAINUNI@users.noreply.github.com>
2022-06-18 13:51:50 -0600

    Update tokens.ts

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -1,13 +1,20 @@
 import { Currency, Ether, NativeCurrency, Token, WETH9 } from '@uniswap/sdk-core'
 import invariant from 'tiny-invariant'
 
-import { UNI_ADDRESS } from './addresses'
+import { DOUGH_ADDRESS } from './addresses'
 import { SupportedChainId } from './chains'
 
 export const USDC_MAINNET = new Token(
   SupportedChainId.MAINNET,
   '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
   6,
+  'USDC',
+  'USD//C'
+)
+export const USDC_BINANCE = new Token(
+  SupportedChainId.BINANCE,
+  '0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d',
+  18,
   'USDC',
   'USD//C'
 )
@@ -67,6 +74,20 @@ export const USDC_ARBITRUM_RINKEBY = new Token(
   'USDC',
   'USD//C'
 )
+export const USDC_FANTOM = new Token(
+  SupportedChainId.FANTOM,
+  '0x04068DA6C83AFCFA0e13ba15A6696662335D5B75',
+  6,
+  'USDC',
+  'USD//C'
+)
+export const USDC_HARMONY = new Token(
+  SupportedChainId.HARMONY,
+  '0x985458e523db3d53125813ed68c274899e9dfab4',
+  6,
+  'USDC',
+  'USD//C'
+)
 export const USDC_POLYGON = new Token(
   SupportedChainId.POLYGON,
   '0x2791bca1f2de4661ed88a30c99a7a9449aa84174',
@@ -81,7 +102,83 @@ export const USDC_POLYGON_MUMBAI = new Token(
   'USDC',
   'USD//C'
 )
-
+export const DOUGH_BINANCE = new Token(
+  SupportedChainId.BINANCE,
+  '0x2f8874d3f92d6d4b52d36e0523e627d70f66b447',
+  9,
+  'DOUGH',
+  'Doughnutswap'
+)
+export const DOUGH_POLYGON = new Token(
+  SupportedChainId.POLYGON,
+  '0x3716D2f01b98B82823e6d1B9a5042C8A287Ef7EF',
+  9,
+  'DOUGH',
+  'Doughnutswap'
+)
+export const JELLY_POLYGON = new Token(
+  SupportedChainId.POLYGON,
+  '0x3D3270E9642E2cF8AE5c3af7f79F63Bb9353F329',
+  9,
+  'JELLY',
+  'Jelly Pool'
+)
+export const TRUSD_BINANCE = new Token(
+  SupportedChainId.BINANCE,
+  '0x9f8462efb1ac6e62f4ebb031afbb7932ba71d05f',
+  18,
+  'TRUSD',
+  'Trust_Dollar'
+)
+export const TRUSD_POLYGON = new Token(
+  SupportedChainId.POLYGON,
+  '0xd17628b62647dfbd5f1b05dab045ff2823213205',
+  18,
+  'TRUSD',
+  'Trust_Dollar'
+)
+export const TRUSD_FANTOM = new Token(
+  SupportedChainId.FANTOM,
+  '0x8ba03ce31f71c6733b0764ce82f3c68d4f6173fa',
+  18,
+  'TRUSD',
+  'Trust_Dollar'
+)
+export const USDa_BINANCE = new Token(
+  SupportedChainId.BINANCE,
+  '0x16EE8F5Fa3D8F6e6C11BB7C0908a0c9E6a6d8e98',
+  9,
+  'USDA',
+  'USDa_Stable_Dollar'
+)
+export const JELLY_BINANCE = new Token(
+  SupportedChainId.BINANCE,
+  '0x39D37f6A0b48dB68E3C5738e88f43A833C5Fd638',
+  9,
+  'JELLY',
+  'Jelly Pool'
+)
+export const DOUGH_HARMONY = new Token(
+  SupportedChainId.HARMONY,
+  '0x302aE52359a5950d97c8e39e763C0706877C4Fa3',
+  9,
+  'DOUGH',
+  'Doughnutswap'
+)
+export const DOUGH_THUNDERCORE = new Token(
+  SupportedChainId.THUNDERCORE,
+  '0xd17628B62647dFbd5f1b05dab045fF2823213205',
+  9,
+  'DOUGH',
+  'Doughnutswap'
+)
+export const DOUGH_FANTOM = new Token(
+  SupportedChainId.FANTOM,
+  '0x4EC22A90b899528156B262e1aF80eBc3f5D7e611',
+  9,
+  'DOUGH',
+  'Doughnutswap'
+)
 export const AMPL = new Token(
   SupportedChainId.MAINNET,
   '0xD46bA6D942050d489DBd938a2C909A5d5039A161',
@@ -122,6 +219,13 @@ export const USDC: { [chainId in SupportedChainId]: Token } = {
   [SupportedChainId.RINKEBY]: USDC_RINKEBY,
   [SupportedChainId.KOVAN]: USDC_KOVAN,
   [SupportedChainId.ROPSTEN]: USDC_ROPSTEN,
+  [SupportedChainId.COINEX]: USDC_COINEX,
+  [SupportedChainId.BINANCE]: USDC_BINANCE,
+  [SupportedChainId.HECO]: USDC_HECO,
+  [SupportedChainId.HARMONY]: USDC_HARMONY,
+  [SupportedChainId.FANTOM]: USDC_FANTOM,
+  [SupportedChainId.AVALANCHE]: USDC_AVALANCHE,
+  [SupportedChainId.GNOSIS]: USDC_GNOSIS,
 }
 export const DAI_POLYGON = new Token(
   SupportedChainId.POLYGON,
@@ -145,9 +249,30 @@ export const WBTC_POLYGON = new Token(
   'Wrapped BTC'
 )
 export const USDT = new Token(
+  SupportedChainId.HARMONY,
+  '0x3C2B8Be99c50593081EAA2A724F0B8285F5aba8f',
+  6,
+  'USDT',
+  'Tether USD'
+)
+export const USDT = new Token(
+  SupportedChainId.FANTOM,
+  '0x049d68029688eabf473097a2fc38ef61633a3c7a',
+  6,
+  'USDT',
+  'Tether USD'
+)
+export const USDT = new Token(
   SupportedChainId.MAINNET,
   '0xdAC17F958D2ee523a2206206994597C13D831ec7',
   6,
+  'USDT',
+  'Tether USD'
+)
+export const USDT = new Token(
+  SupportedChainId.BINANCE,
+  '0x55d398326f99059fF775485246999027B3197955',
+  18,
   'USDT',
   'Tether USD'
 )
@@ -264,12 +389,16 @@ export const WETH_POLYGON = new Token(
   'WETH',
   'Wrapped Ether'
 )
-export const UNI: { [chainId: number]: Token } = {
-  [SupportedChainId.MAINNET]: new Token(SupportedChainId.MAINNET, UNI_ADDRESS[1], 18, 'UNI', 'Uniswap'),
-  [SupportedChainId.RINKEBY]: new Token(SupportedChainId.RINKEBY, UNI_ADDRESS[4], 18, 'UNI', 'Uniswap'),
-  [SupportedChainId.ROPSTEN]: new Token(SupportedChainId.ROPSTEN, UNI_ADDRESS[3], 18, 'UNI', 'Uniswap'),
-  [SupportedChainId.GOERLI]: new Token(SupportedChainId.GOERLI, UNI_ADDRESS[5], 18, 'UNI', 'Uniswap'),
-  [SupportedChainId.KOVAN]: new Token(SupportedChainId.KOVAN, UNI_ADDRESS[42], 18, 'UNI', 'Uniswap'),
+export const USDT: { [chainId: number]: Token } = {
+  [SupportedChainId.MAINNET]: new Token(SupportedChainId.MAINNET, USDT_ADDRESS[1], 18, 'USDT', Doughnutswap),
+  [SupportedChainId.RINKEBY]: new Token(SupportedChainId.RINKEBY, USDT_ADDRESS[4], 18, 'USDT', Doughnutswap),
+  [SupportedChainId.ROPSTEN]: new Token(SupportedChainId.ROPSTEN, USDT_ADDRESS[3], 18, 'USDT', Doughnutswap),
+  [SupportedChainId.GOERLI]: new Token(SupportedChainId.GOERLI, USDT_ADDRESS[5], 18, 'USDT', Doughnutswap),
+  [SupportedChainId.KOVAN]: new Token(SupportedChainId.KOVAN, USDT_ADDRESS[42], 18, 'USDT', Doughnutswap),
+  [SupportedChainId.HARMONY]: new Token(SupportedChainId.HARMONY, USDT_ADDRESS[1666600000], 6, 'USDT', Doughnutswap),
+  [SupportedChainId.BINANCE]: new Token(SupportedChainId.BINANCE, USDT_ADDRESS[56], 18, 'USDT', Doughnutswap),
+  [SupportedChainId.THUNDERCORE]: new Token(SupportedChainId.THUNDERCORE, USDT_ADDRESS[108], 18, 'USDT', Doughnutswap),
+  [SupportedChainId.FANTOM]: new Token(SupportedChainId.FANTOM, USDT_ADDRESS[250], 18, 'USDT', Doughnutswap),
 }
 
 export const WRAPPED_NATIVE_CURRENCY: { [chainId: number]: Token | undefined } = {
@@ -309,12 +438,33 @@ export const WRAPPED_NATIVE_CURRENCY: { [chainId: number]: Token | undefined } =
     'WMATIC',
     'Wrapped MATIC'
   ),
-  [SupportedChainId.POLYGON_MUMBAI]: new Token(
-    SupportedChainId.POLYGON_MUMBAI,
+  [SupportedChainId.HARMONY]: new Token(
+    SupportedChainId.HARMONY,
+    '0xcF664087a5bB0237a0BAd6742852ec6c8d69A27a',
+    18,
+    'WONE',
+    'Wrapped ONE'
+  ),
+  [SupportedChainId.THUNDERCORE]: new Token(
+    SupportedChainId.THUNDERCORE,
+    '0x413cEFeA29F2d07B8F2acFA69d92466B9535f717',
+    18,
+    'wTT',
+    'Wrapped Thunder Token'
+  ),
+  [SupportedChainId.BINANCE]: new Token(
+    SupportedChainId.BINANCE,
+    '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+    18,
+    'WBNB',
+    'Wrapped BNB'
+  ),
+  [SupportedChainId.FANTOM]: new Token(
+    SupportedChainId.FANTOM,
     '0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889',
     18,
-    'WMATIC',
-    'Wrapped MATIC'
+    'WFTM',
+    'Wrapped FTM'
   ),
 }
 
@@ -367,6 +517,15 @@ export function nativeOnChain(chainId: number): NativeCurrency {
 export const TOKEN_SHORTHANDS: { [shorthand: string]: { [chainId in SupportedChainId]?: string } } = {
   USDC: {
     [SupportedChainId.MAINNET]: USDC_MAINNET.address,
+    [SupportedChainId.BINANCE]: USDC_BINANCE.address,
+    [SupportedChainId.FANTOM]: USDC_FANTOM.address,
+    [SupportedChainId.AVALANCHE]: USDC_AVALANCHE.address,
+    [SupportedChainId.HUOBI]: USDC_HUOBI.address,
+    [SupportedChainId.COINEX]: USDC_COINEX.address,
+    [SupportedChainId.HARMONY]: USDC_HARMONY.address,
+    [SupportedChainId.CELO]: USDC_CELO.address,
+    [SupportedChainId.MOONRIVER]: USDC_MOONRIVER.address,
+    [SupportedChainId.THUNDERCORE]: USDC_THUNDERCORE.address,
     [SupportedChainId.ARBITRUM_ONE]: USDC_ARBITRUM.address,
     [SupportedChainId.OPTIMISM]: USDC_OPTIMISM.address,
     [SupportedChainId.ARBITRUM_RINKEBY]: USDC_ARBITRUM_RINKEBY.address,

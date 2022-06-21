480487c1531abfeb525bb260482a6e0d302e811d
SHIBAINUNI <94232115+SHIBAINUNI@users.noreply.github.com>
2022-06-20 22:28:39 -0600

    Update addresses.ts

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -16,6 +16,7 @@ export const MULTICALL_ADDRESS: AddressMap = {
     SupportedChainId.POLYGON,
     SupportedChainId.BINANCE,
     SupportedChainId.HARMONY,
+    SupportedChainId.FANTOM,
   ]),
   [SupportedChainId.ARBITRUM_ONE]: '0xadF885960B47eA2CD9B55E6DAc6B42b7Cb2806dB',
   [SupportedChainId.ARBITRUM_RINKEBY]: '0xa501c031958F579dB7676fF1CE78AD305794d579',
@@ -34,6 +35,7 @@ export const V3_ROUTER_ADDRESS: AddressMap = constructSameAddressMap('0x7f2f2570
   SupportedChainId.THUNDERCORE,
   SupportedChainId.BINANCE,
   SupportedChainId.HARMONY,
+  SupportedChainId.FANTOM,
 ])
 export const SWAP_ROUTER_ADDRESSES: AddressMap = constructSameAddressMap('0x7f2f2570ad2c9a1835745AfD8fc8cc73517FA1cE', [
   SupportedChainId.OPTIMISM,
@@ -45,25 +47,26 @@ export const SWAP_ROUTER_ADDRESSES: AddressMap = constructSameAddressMap('0x7f2f
   SupportedChainId.THUNDERCORE,
   SupportedChainId.BINANCE,
   SupportedChainId.HARMONY,
+  SupportedChainId.FANTOM,
 ])
 
 /**
  * The oldest V0 governance address
  */
 export const GOVERNANCE_ALPHA_V0_ADDRESSES: AddressMap = constructSameAddressMap(
-  '0xc831BE0Bf3CF0116f7b4012f14edB93A2F17bc00'
+  '0x5F83a49aC970f3Cc58f1eca3F0a27fFE20f8299E'
 )
 /**
  * The older V1 governance address
  */
 export const GOVERNANCE_ALPHA_V1_ADDRESSES: AddressMap = {
-  [SupportedChainId.MAINNET]: '0xc831BE0Bf3CF0116f7b4012f14edB93A2F17bc00',
+  [SupportedChainId.MAINNET]: '0xC4e172459f1E7939D522503B81AFAaC1014CE6F6',
 }
 /**
  * The latest governor bravo that is currently admin of timelock
  */
 export const GOVERNANCE_BRAVO_ADDRESSES: AddressMap = {
-  [SupportedChainId.MAINNET]: '0x96e272d65EAe9b18470B1f4425278C7abB6D5f88',
+  [SupportedChainId.MAINNET]: '0x408ED6354d4973f66138C91495F2f2FCbd8724C3',
 }
 
 export const TIMELOCK_ADDRESS: AddressMap = constructSameAddressMap('0x96e272d65EAe9b18470B1f4425278C7abB6D5f88')
@@ -84,6 +87,7 @@ export const V3_CORE_FACTORY_ADDRESSES: AddressMap = constructSameAddressMap(V3_
   SupportedChainId.THUNDERCORE,
   SupportedChainId.BINANCE,
   SupportedChainId.HARMONY,
+  SupportedChainId.FANTOM,
 ])
 export const QUOTER_ADDRESSES: AddressMap = constructSameAddressMap('0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6', [
   SupportedChainId.OPTIMISM,
@@ -95,9 +99,10 @@ export const QUOTER_ADDRESSES: AddressMap = constructSameAddressMap('0xb27308f9F
   SupportedChainId.THUNDERCORE,
   SupportedChainId.BINANCE,
   SupportedChainId.HARMONY,
+  SupportedChainId.FANTOM,
 ])
 export const NONFUNGIBLE_POSITION_MANAGER_ADDRESSES: AddressMap = constructSameAddressMap(
-  '0xC36442b4a4522E871399CD717aBDD847Ab11FE88',
+  '0x4B5EE301DC26d9e6233EC0ffa78442D2DD7DaA0c',
   [
     SupportedChainId.OPTIMISM,
     SupportedChainId.OPTIMISTIC_KOVAN,
@@ -108,6 +113,7 @@ export const NONFUNGIBLE_POSITION_MANAGER_ADDRESSES: AddressMap = constructSameA
     SupportedChainId.THUNDERCORE,
     SupportedChainId.BINANCE,
     SupportedChainId.HARMONY,
+    SupportedChainId.FANTOM,
   ]
 )
 export const ENS_REGISTRAR_ADDRESSES: AddressMap = {
@@ -117,6 +123,8 @@ export const ENS_REGISTRAR_ADDRESSES: AddressMap = {
   [SupportedChainId.RINKEBY]: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
   [SupportedChainId.BINANCE]: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
   [SupportedChainId.HARMONY]: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
+  [SupportedChainId.THUNDERCORE]: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
+  [SupportedChainId.FANTOM]: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
 }
 export const SOCKS_CONTROLLER_ADDRESSES: AddressMap = {
   [SupportedChainId.MAINNET]: '0x65770b5283117639760beA3F867b69b3697a91dd',
@@ -131,6 +139,7 @@ export const V3_MIGRATOR_ADDRESSES: AddressMap = constructSameAddressMap('0xA564
   SupportedChainId.POLYGON,
   SupportedChainId.BINANCE,
   SupportedChainId.HARMONY,
+  SupportedChainId.FANTOM,
 ])
 
 export const TICK_LENS_ADDRESSES: AddressMap = {
